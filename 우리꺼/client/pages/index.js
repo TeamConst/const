48d36d5544664cf4041449d79b1b6f1f9dd5df60
pjh94 <andy3638@naver.com>
2022-03-14 16:26:18 +0900

    Aa.js만 변경되었음

--- 우리꺼/client/pages/index.js
+++ 우리꺼/client/pages/index.js
@@ -1,26 +1,28 @@
 import Layout from "../components/layout";
 import NestedLayout from "../components/nested-layout";
-import Web3Provider from '../store/Web3Provider';
-import CollectionProvider from '../store/CollectionProvider';
-import MarketplaceProvider from '../store/MarketplaceProvider';
+import Web3Provider from "../store/Web3Provider";
+import CollectionProvider from "../store/CollectionProvider";
+import MarketplaceProvider from "../store/MarketplaceProvider";
 
+// import "../styles/globals.css";
+import web3 from "../connection/web3";
+import React, { useContext, useEffect } from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Web3Context from "../store/web3-context";
+import CssBaseline from "@mui/material/CssBaseline";
+import CollectionContext from "../store/collection-context";
+import MarketplaceContext from "../store/marketplace-context";
+import NFTCollection from "../../abis/NFTCollection.json";
+import NFTMarketplace from "../../abis/NFTMarketplace.json";
 
-const Page = () => {
-
-  return;
-};
+const Page = () => {};
 
 Page.getLayout = function getLayout(page) {
   return (
-    // <Web3Provider>
-    // <CollectionProvider>
-    // <MarketplaceProvider>
     <Layout>
       <NestedLayout>{page}</NestedLayout>
     </Layout>
-    // </MarketplaceProvider>
-    // </CollectionProvider>
-    // </Web3Provider>
   );
 };
 
