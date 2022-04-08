c6a618d0abf2e4d022aca91768f60586a5db299e
pjh94 <andy3638@naver.com>
2022-04-08 17:52:08 +0900

    트러플 배포 테스트 중

--- client/components/header.js
+++
@@ -1,7 +0,0 @@
-// 헤더 푸터는 나중에 써줄랭
-
-const Header = () => {
-  return <div>1</div>;
-};
-
-export default Header;
--- client/hooks/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./mypages";
-export * from "./collections";
-export * from "./buysell";
-export * from "./bestCollections";
-export * from "./music";
-export * from "./buy";
-export * from "./getNFT";
