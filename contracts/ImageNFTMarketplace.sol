a92ce119283d4017828af96444500ca32d25db34
pjh94 <andy3638@naver.com>
2022-04-15 12:00:43 +0900

    abi 업로드

---
+++ contracts/ImageNFTMarketplace.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// import "./ImageNFT.sol";
+import "./ImageNFTAuction.sol";
+
+contract ImageMarketplace is ImageAuction {
+    address internal IMAGE_NFT_MARKETPLACE;
+
+    constructor() {
+        IMAGE_NFT_MARKETPLACE = payable(address(this));
+    }
+
+    function mintImageNFT(
+        string memory imageName,
+        string memory ipfsHashOfPhoto
+    ) external returns (bool) {
+        string memory tokenURI = getTokenURI(ipfsHashOfPhoto); /// [Note]: IPFS hash + URL
+        mint(msg.sender, imageName, tokenURI);
+        return true;
+    }
+
+    function baseTokenURI() internal pure returns (string memory) {
+        return "https://ipfs.io/ipfs/";
+    }
+
+    function getTokenURI(string memory _ipfsHashOfPhoto)
+        internal
+        pure
+        returns (string memory)
+    {
+        return string(abi.encodePacked(baseTokenURI(), _ipfsHashOfPhoto));
+    }
+}
