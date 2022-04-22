b61bdbbe0bc28b26fe1ec732eae4a0d900f0faf3
pjh94 <andy3638@naver.com>
2022-04-22 13:05:54 +0900

    회원가입 프로필 이미지 추가, 마이페이지 프로필 이미지 변경 추가

--- client/components/Mypage/mypage.js
+++ client/components/Mypage/mypage.js
@@ -35,7 +35,6 @@ import GetMyNFTDB from "../GetLocalDB/Mine/getMyNFTDB";
 import GetMyBuyDB from "../GetLocalDB/Mine/getMyBuyDB";
 import GetMyAuctionDB from "../GetLocalDB/Mine/getMyAuctionDB";
 //
-import Web3 from "web3";
 //
 import { fetchUserSession, fetchMyNFTDB } from "../../hooks";
 
@@ -98,29 +97,6 @@ const Mypage1 = () => {
     border-radius: 50%;
     margin-top: 75px;
   `;
-  const theme = createTheme();
-
-  async function setupWeb3() {
-    console.log("hihid");
-    if (window.ethereum) {
-      console.log("확인");
-      window.web3 = new Web3(window.ethereum);
-      // Request account access if needed
-      window.ethereum.send("eth_requestAccounts");
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-      // Use Mist/MetaMask's provider.
-      window.web3 = new Web3(window.web3.currentProvider);
-      console.log("주입된 web3가 감지되었습니다.");
-    }
-    // Fallback to localhost; use dev console port by default...
-    else {
-      console.alert(
-        "Infura/Local web3를 사용하여 주입된 web3 인스턴스가 없습니다."
-      );
-    }
-  }
 
   const data = {
     name: "Rita Correia",
@@ -131,112 +107,6 @@ const Mypage1 = () => {
     photos: "1.4K",
   };
   console.log(data.name);
-
-  const [accountAddress, setAccountAddress] = useState("");
-  const [accountBalance, setAccountBalance] = useState("");
-  const [Contract, setContract] = useState(null);
-  const [ImageCount, setImageCount] = useState(0);
-  const [Images, setImages] = useState([]);
-  const [ImageNumOfAccount, setImageNumOfAccount] = useState(0);
-  const [lastMintTime, setLastMintTime] = useState(null);
-  const [Auctions, setAuctions] = useState([]);
-  const [currentTime, setCurrentTime] = useState(null);
-  const [ready, setReady] = useState(false);
-
-  const [b, seta] = useState(true);
-
-  const setupBlockchain = async () => {
-    seta(false);
-    let ImageNFTMarketplace = {};
-
-    try {
-      ImageNFTMarketplace = require("../../../build/contracts/ImageMarketplace.json");
-    } catch (e) {
-      console.log(e);
-    }
-    try {
-      // 네트워크 공급자 및 web3 인스턴스를 가져옵니다.
-      const web3 = window.web3;
-      const accounts = await web3.eth.getAccounts();
-
-      console.log(accounts);
-      // Get the contract instance.
-      let balance =
-        accounts.length > 0
-          ? await web3.eth.getBalance(accounts[0])
-          : await web3.utils.toWei("0");
-      balance = await web3.utils.fromWei(balance, "ether");
-
-      console.log("balance", balance);
-
-      const networkId = await web3.eth.net.getId();
-      let NFTMarketplaceInstance = null;
-      let deployedNetwork = null;
-
-      // Create instance of contracts
-
-      console.log("networkId", networkId);
-
-      if (ImageNFTMarketplace.networks) {
-        deployedNetwork = ImageNFTMarketplace.networks[networkId];
-        if (deployedNetwork) {
-          NFTMarketplaceInstance = new web3.eth.Contract(
-            ImageNFTMarketplace.abi,
-            deployedNetwork.address
-          );
-        }
-      }
-      console.log("ImageNFTMarketplace", ImageNFTMarketplace);
-      console.log("deployedNetwork", deployedNetwork);
-      console.log("NFTMarketplaceInstance", NFTMarketplaceInstance);
-      if (NFTMarketplaceInstance) {
-        const ImageCount = await NFTMarketplaceInstance.methods
-          .currentImageCount()
-          .call();
-        console.log(ImageCount);
-        for (let i = 1; i <= ImageCount; i++) {
-          let image = await NFTMarketplaceInstance.methods
-            .imageStorage(i)
-            .call();
-          setImages((Images) => [...Images, image]);
-          console.log(...Images, image);
-          let auction = await NFTMarketplaceInstance.methods.auctions(i).call();
-          let auction2 = [...Auctions, auction];
-          console.log(auction2);
-          setAuctions((Auctions) => [...Auctions, auction]);
-          console.log(auction.endTime);
-          // console.log("auction",auction);
-          console.log("auctions", Auctions);
-        }
-        console.log(Auctions);
-        let ImageNumOfAccount = await NFTMarketplaceInstance.methods
-          .getOwnedNumber(accounts[0])
-          .call();
-        setContract(NFTMarketplaceInstance);
-        setAccountAddress(accounts[0]);
-        setAccountBalance(balance);
-        setImageCount(ImageCount);
-        setImageNumOfAccount(ImageNumOfAccount);
-        // setReady(true)
-      } else throw "스마트 연락처에 연결하지 못했습니다.";
-    } catch (error) {
-      // 위의 작업에 대한 오류를 포착합니다.
-      alert(
-        "web3, 계정 또는 계약을 로드하지 못했습니다. 자세한 내용은 콘솔을 확인하세요."
-      );
-      console.error(error);
-    }
-  };
-
-  (async function componentWillMount() {
-    if (b == true) {
-      setReady(false);
-      setupWeb3();
-      setupBlockchain();
-    }
-  })();
-
-  // 여기서부터 쓰면 댐 위에 볼 필요 없음
 
   // 컨트랙트 처리 위해서
   const useUser1 = () => {
@@ -252,6 +122,21 @@ const Mypage1 = () => {
   const data1 = useUser1();
   const data2 = useUser2();
 
+  const [accountBalance, setAccountBalance] = useState("");
+
+  useEffect(() => {
+    async function loadBalance() {
+      const accounts = await web3.eth.getAccounts();
+      let balance =
+        accounts.length > 0
+          ? await web3.eth.getBalance(accounts[0])
+          : await web3.utils.toWei("0");
+      balance = await web3.utils.fromWei(balance, "ether");
+      setAccountBalance(balance);
+    }
+    loadBalance();
+  }, []);
+
   let a = 0;
   let userSession;
   if (data1.data) {
@@ -259,11 +144,11 @@ const Mypage1 = () => {
     userSession = data1.data.data;
   }
 
-  let b = 0;
+  let c = 0;
   let myNFTData;
   let count = 0;
   if (data2.data) {
-    b = 1;
+    c = 1;
     myNFTData = data2.data.data;
     count = data2.data.data.length;
   }
@@ -321,15 +206,15 @@ const Mypage1 = () => {
       const image = e.target.files[0];
       const imageFormData = new FormData();
       imageFormData.append("image", image);
-      imageFormData.append("title", image.name);
-      imageFormData.append("CID", image.name);
+      imageFormData.append("id2", userSession.id2);
+
       const resultImage = await axios.post(
-        "http://localhost:8080/api/mint/image",
+        "http://localhost:8080/api/signup/updateImage",
         imageFormData
       );
       const rere = axios.post("http://localhost:8080/api/updateuser2", {
-        profileImg: `https://const123.s3.ap-northeast-2.amazonaws.com/image/${image.name}.jpg`,
-        id: userSession.id,
+        profileImg: `https://const123.s3.ap-northeast-2.amazonaws.com/profile/${userSession.id2}.jpg`,
+        id2: userSession.id2,
       });
     }
     console.log(rere);
@@ -349,18 +234,7 @@ const Mypage1 = () => {
 
         <Container maxWidth="lg">
           <main>
-            <button onClick={claimFundsHandler} className="btn btn-success">
-              구매 완료 된 이더 받기
-            </button>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              버튼버튼
-            </Button>
-            {a === 1 && b === 1 ? (
+            {a === 1 && c === 1 ? (
               <Grid container spacing={5}>
                 <Grid item xs={12}>
                   <Cardcontainer>
@@ -385,18 +259,17 @@ const Mypage1 = () => {
 
                     <Link href={`/userUpdate2`}>
                       <Followers>
-                        {" "}
                         <Smallertext>수정하기</Smallertext>
                       </Followers>
                     </Link>
                     <Boldtext>
-                      {userSession.name}{" "}
+                      {userSession.name}
                       <span className="normal-text">나이/랭크</span>
                     </Boldtext>
                     <Normaltext>{userSession.artist}</Normaltext>
                     <Socialcontainer>
                       <Followers>
-                        <Boldtext>{ImageNumOfAccount}</Boldtext>
+                        <Boldtext>{count}</Boldtext>
                         <Smallertext> 소유한 NFTS의 총 수</Smallertext>
                       </Followers>
                       <Followers className="likes">
@@ -405,7 +278,6 @@ const Mypage1 = () => {
                       </Followers>
                       <Followers className="photos">
                         <Boldtext>
-                          {" "}
                           {`${userSession.ticket}${lastTime}일남음`}
                         </Boldtext>
                         <Smallertext>이용권 정보</Smallertext>
@@ -423,49 +295,54 @@ const Mypage1 = () => {
                     p={2}
                   >
                     <Followers>
-                      {" "}
                       <Boldtext> 내 수익 현황</Boldtext>
                     </Followers>
                   </Cardcontainer>
                 </Grid>
                 <Grid item xs={3}>
-                  {/* <Link href={`/buysell/${encodeURIComponent(a.Key)}`}> */}
-                  <Link href={`/mypage/myNFT/${accountAddress}`}>
+                  <Link href={`/mypage/myNFT/${userSession.address}`}>
                     <Cardcontainer
                       bgcolor="info.main"
                       color="info.contrastText"
                       p={2}
                     >
                       <Followers>
-                        {" "}
-                        <Boldtext>NFT관리하기</Boldtext>
+                        <Boldtext>
+                          <Button
+                            onClick={claimFundsHandler}
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                          >
+                            구매 완료 된 이더 받기
+                          </Button>
+                        </Boldtext>
                       </Followers>
                     </Cardcontainer>
                   </Link>
                 </Grid>
                 <Grid item xs={9}>
-                  <Link href={`/mypage/myNFT/${accountAddress}`}>
+                  <Link href={`/mypage/myNFT/${userSession.address}`}>
                     <Cardcontainer
                       bgcolor="info.main"
                       color="info.contrastText"
                       p={2}
                     >
                       <Followers>
-                        {" "}
-                        <Boldtext>나의 전체 </Boldtext>
+                        <Boldtext>나의 전체 NFT </Boldtext>
                       </Followers>
                     </Cardcontainer>
                   </Link>
                 </Grid>
                 <Grid item xs={3}>
-                  <Link href={`/mypage/myNFT/${accountAddress}`}>
+                  <Link href={`/mypage/myNFT/${userSession.address}`}>
                     <Cardcontainer
                       bgcolor="info.main"
                       color="info.contrastText"
                       p={2}
                     >
                       <Followers>
-                        {" "}
                         <Boldtext> 자세히보기 </Boldtext>
                       </Followers>
                     </Cardcontainer>
@@ -482,20 +359,18 @@ const Mypage1 = () => {
                     p={2}
                   >
                     <Followers>
-                      {" "}
                       <Boldtext> 판매중인 나의 NFT </Boldtext>
                     </Followers>
                   </Cardcontainer>
                 </Grid>
                 <Grid item xs={3}>
-                  <Link href={`/mypage/myBuy/${accountAddress}`}>
+                  <Link href={`/mypage/myBuy/${userSession.address}`}>
                     <Cardcontainer
                       bgcolor="info.main"
                       color="info.contrastText"
                       p={2}
                     >
                       <Followers>
-                        {" "}
                         <Boldtext> 자세히보기 </Boldtext>
                       </Followers>
                     </Cardcontainer>
@@ -513,20 +388,18 @@ const Mypage1 = () => {
                     p={2}
                   >
                     <Followers>
-                      {" "}
                       <Boldtext> 경매중인 나의 NFT </Boldtext>
                     </Followers>
                   </Cardcontainer>
                 </Grid>
                 <Grid item xs={3}>
-                  <Link href={`/mypage/myAuction/${accountAddress}`}>
+                  <Link href={`/mypage/myAuction/${userSession.address}`}>
                     <Cardcontainer
                       bgcolor="info.main"
                       color="info.contrastText"
                       p={2}
                     >
                       <Followers>
-                        {" "}
                         <Boldtext> 자세히 보기 </Boldtext>
                       </Followers>
                     </Cardcontainer>
@@ -544,7 +417,6 @@ const Mypage1 = () => {
                     p={2}
                   >
                     <Followers>
-                      {" "}
                       <Boldtext> 이용권 정보 </Boldtext>
                     </Followers>
                     {`이용권:${userSession.ticket}${lastTime}일남음`}
@@ -557,7 +429,6 @@ const Mypage1 = () => {
                     p={2}
                   >
                     <Followers>
-                      {" "}
                       <Boldtext> 내 음원의 총 재생시간 </Boldtext>
                     </Followers>
                   </Cardcontainer>
@@ -569,7 +440,6 @@ const Mypage1 = () => {
                     p={2}
                   >
                     <Followers>
-                      {" "}
                       <Boldtext> 내음원을 들은 총 재생 횟수 </Boldtext>
                     </Followers>
                   </Cardcontainer>
@@ -581,10 +451,8 @@ const Mypage1 = () => {
                     p={2}
                   >
                     <Followers>
-                      {" "}
                       <Boldtext>
-                        {" "}
-                        내가 최근 들은 곡 Recently Played 등등{" "}
+                        내가 최근 들은 곡 Recently Played 등등
                       </Boldtext>
                     </Followers>
                   </Cardcontainer>
